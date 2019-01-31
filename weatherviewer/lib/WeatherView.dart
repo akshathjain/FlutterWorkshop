25479da7bbb94ccdd4a068dbd2ae0b709923bdfa
Akshath Jain <akshath.r.jain@gmail.com>
2019-01-31 17:50:35 -0500

    changed iOS name

---
+++ weatherviewer/lib/WeatherView.dart
@@ -0,0 +1,34 @@
+/*
+Name: Akshath Jain
+Date: 1/31/18
+Purpose: display a cities weatehr
+*/
+
+import 'package:flutter/material.dart';
+import 'APIKey.dart';
+
+class WeatherView extends StatefulWidget{
+  WeatherView({Key key, this.cityId}) : super(key: key);
+  final cityId;
+
+  @override
+  _WeatherViewState createState() => new _WeatherViewState();
+}
+
+class _WeatherViewState extends State<WeatherView>{
+  Map _info;
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+  }
+
+  Future<Map> _fetchInfo() async{
+    var response = await http.get("api.openweathermap.org/data/2.5/weather?id=5206379&appid=" + apikey);
+    return jsonDecode(response.body);
+  }
+}
--- weatherviewer/lib/main.dart
+++ weatherviewer/lib/main.dart
@@ -24,39 +24,13 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      body: WeatherView(),
     );
   }
 }
